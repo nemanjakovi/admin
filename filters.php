02a3bfe36c96e9c0c3a6eb2bed50911d16c3138c
nemanjakovi <91414170+nemanjakovi@users.noreply.github.com>
2022-12-27 05:11:06 +0100

    Update filters.php

--- filters.php
+++ filters.php
@@ -1,35 +1,20 @@
 <?php
 $query = "SELECT * FROM user ";
-$where = "WHERE";
 
-if (!empty($_POST['filter_name'])) {
-    $query .= $where . " " . 'first_name' . ' = "' .  $filterName . '"';
+$conditions = [];
+if (!empty($filterName)) {
+    $conditions[] = ' first_name = ' . "'$filterName'";
 }
-if (!empty($_POST['filter_last_name'])) {
-    $query .=  ' ' .  "AND" . ' last_name' . ' = "' .  $filterLastName . '" ';
+if (!empty($filterLastName)) {
+    $conditions[] = ' last_name = ' . "'$filterLastName'";
+}
+if (!empty($filterGender)) {
+    $conditions[] = ' gender = ' . "'$filterGender'";
+}
+if (!empty($filterDateFrom) && !empty($filterToDate)) {
+    $conditions[] = ' date BETWEEN ' . "'$filterDateFrom' AND " . "'$filterToDate' ";
+}
 
-    if (empty($filterName)) {
-        $query = "SELECT * FROM user ";
-        $query .= $where . " " . 'last_name' . ' = "' .  $filterLastName . '"';
-    }
-}
-if (!empty($_POST["gender"])) {
-    $query .= ' ' .  "AND" . ' gender' . ' = "' .  $filterGender . '" ';
-
-    if (empty($filterName)) {
-        $query = "SELECT * FROM user ";
-        $query .= $where . ' last_name = "'  . $filterLastName . '" AND ' . 'gender' . ' = "' .  $filterGender . '"';
-    }
-    if (empty($filterLastName)) {
-        $query = "SELECT * FROM user ";
-        $query .= $where . ' first_name = "'  . $filterName . '" AND ' . 'gender' . ' = "' .  $filterGender . '"';
-    }
-    if (empty($filterName) && empty($filterLastName)) {
-        $query = "SELECT * FROM user ";
-        $query .= $where . " " . 'gender' . ' = "' .  $filterGender . '"';
-    }
-}
-if (!empty($filterFromDate) && !empty($filterToDate)) {
-    $query = "SELECT * FROM user ";
-    $query .=  " WHERE " . ' date BETWEEN "' . $filterFromDate . '" AND "' . $filterToDate . '"';
+if (count($conditions) > 0) {
+    $query .= " WHERE " . implode(" AND ", $conditions);
 }
